e18f7be542e74a320f91a91df9e838c3313e55f2
Amit Singh <iamitsinghthakur123@gmail.com>
2024-04-11 12:29:23 +0530

    intial push

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+app.use(cors());
+
+let medicineList = [];
+
+app.post("/medicine", (req, res) => {
+  const medicine = req.body.medicine;
+  // console.log(req);
+  const medicineName = medicine.split(" ").join("+") + "+tablet";
+  const API = `${process.env.API_ENDPOINT}${medicineName}`;
+
+  fetch(API)
+    .then((data) => data.json())
+    .then((data) => {
+      const medicineData = data.pageProps.serverData.medData.result;
+      //   res.send(medicineData);
+      medicineData.map((item) => {
+        medicineList.push({
+          medicine_name: item._source.Medicine_Name,
+          medicine_link: item._source.Medicine_Link,
+          retail_price: item._source.Retail_Price,
+          discounted_price: item._source.Discounted_Price,
+          image:
+            item._source.Images[0] ||
+            "https://www.kauverymeds.com/uploads/product/main/thumb.png",
+          company: item._source.Source,
+        });
+      });
+      res.render("compare", { medicineList: medicineList });
+      medicineList = [];
+      // console.log(medicineList);
+      // res.send(medicineList);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Port 3000 is running...");
+});
